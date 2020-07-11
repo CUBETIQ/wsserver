22909e22046ae8298fe8b6bcffb399d8fad04153
Sambo Chea <sombochea@cubetiqs.com>
2020-07-11 19:47:15 +0700

    Init wsserver project

---
+++ src/test/kotlin/com/cubetiqs/wsserver/WsserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cubetiqs.wsserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WsserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
