bc4005a1a33510d065eead0c45d89e2bbb361873
Sambo Chea <sombochea@cubetiqs.com>
2020-07-11 21:02:26 +0700

    Updated and add tests and services

--- src/test/kotlin/com/cubetiqs/wsserver/WsServerApplicationTests.kt
+++ src/test/kotlin/com/cubetiqs/wsserver/WsServerApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class WsserverApplicationTests {
+class WsServerApplicationTests {
 
 	@Test
 	fun contextLoads() {
